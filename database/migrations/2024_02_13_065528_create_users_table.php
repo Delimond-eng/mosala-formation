ed0c4ad1915261339b344834cf8b95d571abb417
Delimond-eng <gastondelimond@gmail.com>
2025-04-04 21:25:46 +0100

    update

---
+++ database/migrations/2024_02_13_065528_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('profile_id')->nullable();
+            $table->string('profile_type')->nullable();
+            $table->timestamp('user_created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
